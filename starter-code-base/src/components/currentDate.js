28e98adf8b4b659154029a5fe3a6f8f22a8fa793
Krystal <krystalkoh@gmail.com>
2022-07-24 21:59:36 +0800

    Update currentDate.js

--- starter-code-base/src/components/currentDate.js
+++ starter-code-base/src/components/currentDate.js
@@ -4,5 +4,21 @@ export function getCurrentDate(separator = "") {
   let month = newDate.getMonth() + 1;
   let year = newDate.getFullYear();
 
-  return `${year}-${month < 10 ? `0${month}` : `${month}`}-${date}`;
+  let finalDate = "";
+  if (date <= 9) {
+    finalDate = `${year}-${month < 10 ? `0${month}` : `${month}`}-0${date}`;
+  } else {
+    finalDate = `${year}-${month < 10 ? `0${month}` : `${month}`}-${date}`;
+  }
+
+  return finalDate;
 }
+
+// export function getCurrentDate(separator = "") {
+//   let newDate = new Date();
+//   let date = newDate.getDate();
+//   let month = newDate.getMonth() + 1;
+//   let year = newDate.getFullYear();
+
+//   return `${year}-${month < 10 ? `0${month}` : `${month}`}-${date}`;
+// }
